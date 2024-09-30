f5e1918f35e3eedd1d351ee9a294a02fcfce1642
TapZe <nabilmuyassar@Gmail.com>
2024-09-30 20:25:42 +0800

    style: fixing main layout tag

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,13 +4,13 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <container className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex flex-grow">
         <Outlet />
       </main>
       <Footer />
-    </container>
+    </div>
   );
 };
 
