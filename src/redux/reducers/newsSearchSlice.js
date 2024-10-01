111cb1c839d06f91a6864db6e0afe1030e7e7da6
TapZe <nabilmuyassar@Gmail.com>
2024-10-01 10:24:25 +0800

    fix: renaming variable in redux fetching

--- src/redux/reducers/newsSearchSlice.js
+++ src/redux/reducers/newsSearchSlice.js
@@ -5,7 +5,7 @@ import { SEARCH_URI } from "../../constants/apiBaseURI";
 export const newsSearchSlice = createSlice({
   name: "newsSearchSlice",
   initialState: {
-    data: [],
+    news: [],
     isLoading: false,
     errorMessage: "",
   },
@@ -13,7 +13,7 @@ export const newsSearchSlice = createSlice({
     fetchNewsSearchSuccess: (state, action) => {
       state.isLoading = false;
       state.errorMessage = "";
-      state.data = action.payload;
+      state.news = action.payload;
     },
     fetchNewsSearchLoading: (state, action) => {
       state.isLoading = action.payload;
@@ -33,21 +33,25 @@ export const {
 
 export default newsSearchSlice.reducer;
 
-export function fetchNews(query = "indonesia") {
+export function fetchNews(query = "indonesia", begin_date, end_date) {
   return async (dispatch /*, getState*/) => {
     const { VITE_NYT_API_KEY } = import.meta.env;
+    const params = {
+      "api-key": VITE_NYT_API_KEY,
+      query,
+    };
+    if (begin_date) params.begin_date = begin_date;
+    if (end_date) params.end_date = end_date;
+
     dispatch(fetchNewsSearchLoading(true));
     try {
       const { data } = await axios({
         method: "GET",
         url: SEARCH_URI,
-        params: {
-          "api-key": VITE_NYT_API_KEY,
-          query,
-        },
+        params,
       });
-      const { docs: news } = await data.response;
-      dispatch(fetchNewsSearchSuccess(news));
+      const { docs: newsData } = await data.response;
+      dispatch(fetchNewsSearchSuccess(newsData));
     } catch (error) {
       dispatch(fetchNewsSearchError(error.message));
     } finally {
