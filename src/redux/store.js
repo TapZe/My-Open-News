3bacb2f7b7bfc408cc19e5ee3cdd8966f6d83ea9
TapZe <nabilmuyassar@Gmail.com>
2024-10-02 12:09:07 +0800

    feat: setting the reducers and slice for theme

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
-// import { combineReducers } from "redux";
+import { combineReducers } from "redux";
 
 import newsSearchReducer from "./reducers/newsSearchSlice";
 import savedNewsReducer from "./reducers/savedNewsSlice";
+import themeReducer from "./reducers/themeSlice";
 
 const persistConfig = {
   key: "root",
@@ -12,13 +13,14 @@ const persistConfig = {
 };
 
 // If the reducers that need persist is more than one
-// const rootReducer = combineReducers({
-//   newsSearchReducer,
-//   savedNewsReducer,
-// });
+const rootReducer = combineReducers({
+  theme: themeReducer,
+  savedNews: savedNewsReducer,
+});
 
-// place the rootReducer at the "savedNewsReducers" for multiple reducers persist
-const persistedReducer = persistReducer(persistConfig, savedNewsReducer);
+// for only one reducer needs persist, rootReducer can be replaced with the one reducer that needs it.
+// it is recommended for future coding that it's saved in a root reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: {
@@ -28,7 +30,7 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        // Ignore actions and paths where non-serializable values might exist
+        // Ignore actions and paths where non-serializable values might exist for persist
         ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
       },
     }),
