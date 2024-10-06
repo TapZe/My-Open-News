9bb7abb5a7c00257f440e47287bb21ce6217be82
TapZe <nabilmuyassar@Gmail.com>
2024-10-06 14:20:37 +0800

    feat: save news button tooltip update, added NewsSaveBtn component

---
+++ src/components/news/cards/NewsSaveBtn.jsx
@@ -0,0 +1,46 @@
+import { faBookmark as solidBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faBookmark as regularBookmark } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+const NewsSaveBtn = ({ isSaved, handleSave }) => {
+  const [tooltipMessage, setTooltipMessage] = useState("");
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  // function to handle button click and show tooltip with animation
+  const handleClick = () => {
+    const newState = !isSaved; // Determine new state after save/remove
+    setTooltipMessage(newState ? "Saved!" : "Removed!");
+    setShowTooltip(true);
+
+    // Hide the tooltip after 2 seconds
+    setTimeout(() => setShowTooltip(false), 2000);
+
+    // Call the parent handleSave function to toggle the saved state
+    handleSave();
+  };
+
+  return (
+    <div className="group absolute top-2 right-2">
+      <button className="btn btn-sm btn-secondary" onClick={handleClick}>
+        <FontAwesomeIcon icon={isSaved ? solidBookmark : regularBookmark} />
+      </button>
+      <div
+        className={`tooltip absolute left-1/2 bottom-full mb-2 hidden w-32 -translate-x-1/2 rounded-md bg-gray-700 text-white text-xs px-2 py-1 shadow-lg group-hover:block ${
+          showTooltip ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        {isSaved ? `Remove This Article` : `Save This Article`}
+      </div>
+      <div
+        className={`tooltip absolute left-1/2 bottom-full mb-2 w-32 -translate-x-1/2 rounded-md text-xs px-2 py-1 shadow-lg z-50 transition-opacity duration-300 ${
+          showTooltip ? "opacity-100" : "opacity-0"
+        } ${isSaved ? "bg-success text-black" : "bg-error text-black"}`}
+      >
+        {tooltipMessage}
+      </div>
+    </div>
+  );
+};
+
+export default NewsSaveBtn;
