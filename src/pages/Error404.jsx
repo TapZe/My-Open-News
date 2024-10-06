2f6fc7a490cec4399dfc7830fcf70af678cd5224
TapZe <nabilmuyassar@Gmail.com>
2024-10-06 14:20:05 +0800

    style: Error page responsive update

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -4,16 +4,24 @@ const Error404 = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen flex justify-center items-center bg-error">
-      <div className="text-center">
-        <h1 className="text-9xl font-bold text-error-content">404</h1>
-        <h2 className="text-3xl font-bold text-white mb-4">Page Not Found!</h2>
-        <p className="text-lg text-white mb-8">
-          The page you're looking for doesn't exist.
-        </p>
-        <button className="btn btn-primary btn-lg" onClick={() => navigate(-1)}>
-          Go Back
-        </button>
+    <div className="min-h-screen flex flex-col">
+      <div className="flex flex-grow justify-center items-center bg-error bg-gradient-to-r from-error-500 to-error-700">
+        <div className="container max-w-md p-4 mx-auto text-center">
+          <h1 className="text-9xl font-bold text-error-content mb-4">404</h1>
+          <h2 className="text-3xl font-bold text-white mb-2">
+            Page Not Found!
+          </h2>
+          <p className="text-lg text-white mb-8">
+            The page you're looking for doesn't exist. Try checking the URL or
+            going back to the previous page.
+          </p>
+          <button
+            className="btn btn-primary btn-lg w-full md:w-auto mb-4"
+            onClick={() => navigate(-1)}
+          >
+            Go Back
+          </button>
+        </div>
       </div>
     </div>
   );
