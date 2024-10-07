ec23d6147debaa3764a758cf07d66659e2d0e670
TapZe <nabilmuyassar@Gmail.com>
2024-10-07 12:53:30 +0800

    feat: preparing home page grid and layout

---
+++ src/components/news/NewsHomeGrid.jsx
@@ -0,0 +1,51 @@
+import { useSelector } from "react-redux";
+import NewsCard from "./cards/NewsCard";
+import NewsCardSkeleton from "./cards/NewsCardSkeleton";
+
+const NewsHomeGrid = () => {
+  const { news, isLoading } = useSelector((state) => state.newsSearch);
+
+  // This is for defining how many copies of skeleton
+  const skeletonCount = 8;
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {Array(skeletonCount)
+          .fill(null)
+          .map((_, index) => (
+            <NewsCardSkeleton key={index} />
+          ))}
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="grid grid-col-1 lg:grid-cols-2 gap-8">
+        {news[0] && (
+          <div className="lg:row-span-1">
+            <NewsCard key={news[0]._id} article={news[0]} showLead={true} />
+          </div>
+        )}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:row-span-2">
+          {news?.slice(1, 5).map((article) => (
+            <NewsCard key={article._id} article={article} />
+          ))}
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:row-start-2 lg:row-span-2">
+          {news?.slice(5, 9).map((article) => (
+            <NewsCard key={article._id} article={article} />
+          ))}
+        </div>
+        {news[9] && (
+          <div className="lg:row-span-1">
+            <NewsCard key={news[9]._id} article={news[9]} showLead={true} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default NewsHomeGrid;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,40 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchNews, setPage } from "../redux/reducers/newsSearchSlice";
+import NewsHomeGrid from "../components/news/NewsHomeGrid";
+
 const Home = () => {
+  const { page } = useSelector((state) => state.newsSearch);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // setting the parameters
+    const params = {
+      query: "",
+      page,
+    };
+    const promise = dispatch(fetchNews(params));
+    return () => {
+      // `createAsyncThunk` attaches an `abort()` method to the promise
+      promise.abort();
+    };
+  }, [page]);
+
+  // Reset page when unmounted only
+  useEffect(() => {
+    return () => {
+      dispatch(setPage(0));
+    };
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to my home page</h1>
-    </div>
+    <>
+      <h1 className="text-3xl font-bold text-center mb-10">
+        Welcome to <span className="text-cyan-600">O</span>pen
+        <span className="text-cyan-600">News</span>
+      </h1>
+      <NewsHomeGrid />
+    </>
   );
 };
 
