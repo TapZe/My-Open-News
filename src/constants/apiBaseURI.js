94e09c82a943d323ed35ff5beb66f2c57c3e0e09
TapZe <nabilmuyassar@Gmail.com>
2024-10-07 11:02:46 +0800

    chore: setting up api url default parameter

--- src/constants/apiBaseURI.js
+++ src/constants/apiBaseURI.js
@@ -1,9 +1,9 @@
 const BASE_URI = import.meta.env.VITE_NYT_API_BASE_URI;
 
 export const SEARCH_URI = `${BASE_URI}/search/v2/articlesearch.json`;
-export const TOP_STORY_URI = (section) => {
+export const TOP_STORY_URI = (section = "home") => {
   return `${BASE_URI}/topstories/v2/${section}.json`;
 };
-export const TIMEWIRE_URI = (section) => {
+export const TIMEWIRE_URI = (section = "home page") => {
   return `${BASE_URI}/news/v3/content/all/${section}.json`;
 };
