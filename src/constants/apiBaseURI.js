1a1f09beaa63fc34867f4e8d3600dd573771ab18
TapZe <nabilmuyassar@Gmail.com>
2024-10-07 12:50:41 +0800

    chore: disabling unused api uri

--- src/constants/apiBaseURI.js
+++ src/constants/apiBaseURI.js
@@ -4,6 +4,8 @@ export const SEARCH_URI = `${BASE_URI}/search/v2/articlesearch.json`;
 export const TOP_STORY_URI = (section = "home") => {
   return `${BASE_URI}/topstories/v2/${section}.json`;
 };
-export const TIMEWIRE_URI = (section = "home page") => {
-  return `${BASE_URI}/news/v3/content/all/${section}.json`;
-};
+
+/// Unused api url
+// export const TIMEWIRE_URI = (section) => {
+//   return `${BASE_URI}/news/v3/content/all/${section}.json`;
+// };
